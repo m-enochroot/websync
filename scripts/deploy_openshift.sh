bb4f79d96f232238421b28305f88fc5def845129
jerome-mare <jerome.mare@gfi.fr>
2016-04-04 10:45:18 +0200

    add origin remote for openshift deployment

--- scripts/deploy_openshift.sh
+++ scripts/deploy_openshift.sh
@@ -17,5 +17,6 @@ git config --global push.default simple
 git config --global user.email "travis@travis-ci.com"
 git config --global user.name "Travis CI"
 git checkout -b master
+git remote add origin ssh://56c8f70b0c1e66171300004d@gateway-gfitls.rhcloud.com/~/git/gateway.git
 git add --all
 git commit -am "Travis deploy"
