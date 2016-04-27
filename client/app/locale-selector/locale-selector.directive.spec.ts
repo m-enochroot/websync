ad5aa4420b05cffdd3e8ec6f0e2774dfa9cbaf8a
jerome-mare <jerome.mare@gfi.fr>
2016-04-27 09:16:16 +0200

    Fix unit test

--- client/app/locale-selector/locale-selector.directive.spec.ts
+++ client/app/locale-selector/locale-selector.directive.spec.ts
@@ -7,15 +7,25 @@ describe('Directive: localeSelector', function () {
   beforeEach(module('app/locale-selector/locale-selector.html'));
 
   var element, scope;
+  var $httpBackend
 
-  beforeEach(inject(function ($rootScope) {
+  beforeEach(inject(function ($rootScope, _$httpBackend_) {
     scope = $rootScope.$new();
+    $httpBackend = _$httpBackend_;
+    var i18nRes = {
+        languageNames: {
+            en: 'Language',
+            es: 'Language',
+            fr: 'Language'
+        }
+      };
+    $httpBackend.expectGET(/\/api\/lang\?lang=(en|fr|es)/).respond(200, i18nRes);
   }));
 
   it('should make hidden element visible', inject(function ($compile) {
     element = angular.element('<locale-selector></locale-selector>');
     element = $compile(element)(scope);
     scope.$apply();
-    expect(element.text()).to.equal('this is the localeSelector directive');
+    expect(element.text()).to.equal('Language');
   }));
 });
